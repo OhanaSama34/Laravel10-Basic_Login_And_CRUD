5ebc4e1d314d822789c1cec860ed2201e6dbd50f
OhanaSama34 <noufalgamer124@gmail.com>
2024-01-30 13:46:47 +0700

    Laravel 10 CRUD Basic

---
+++ app/Http/Requests/Post/StoreRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|min:3|max:250',
+            'content' => 'required|string|min:3|max:6000',
+            'cover_image' => 'required|image|min:3|max:10240|mimes:jpg,jpeg,psd,png,gif',
+
+        ];
+    }
+}
